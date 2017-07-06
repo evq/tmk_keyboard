19a6bc332829f2ccafed26849a0caa4b05a3929f
eV <ev@7pr.xyz>
2017-07-06 05:38:00 +0000

    add my config

--- keyboard/alps64/keymap_plain.c
+++ keyboard/alps64/keymap_plain.c
@@ -5,8 +5,8 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP( \
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, NUHS, BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
-        LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,ESC, \
-        LCTL,LGUI,LALT,          SPC,                     APP, RALT,RGUI,RCTL),
+        ESC, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
+        LSFT,NUBS,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,UP,ESC, \
+        LCTL,LALT,LGUI,          SPC,                     APP, LEFT,DOWN,RGHT),
 };
 const uint16_t PROGMEM fn_actions[] = {};
